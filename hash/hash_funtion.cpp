330d32ab8f3be6f0deb6a3b132e9534408415c6f
Kacperfis <42153314+Kacperfis@users.noreply.github.com>
2020-08-25 15:26:13 +0200

    Create hash_funtion.cpp

---
+++ hash/hash_funtion.cpp
@@ -0,0 +1,103 @@
+/*																													   
+ Templatka c++ Kacper Fis																							   																											   //
+*/																													  
+																													   
+#include <iostream>       	
+#include <cstdio>         
+#include <limits>		  
+#include <cstdlib>       
+#include <utility>        
+#include <iterator>       
+#include <bitset>         
+#include <list>           
+#include <vector>         
+#include <map>            
+#include <unordered_map>            
+#include <set>            														   
+#include <queue>          																							   
+#include <deque>          																							   
+#include <stack>																									   
+#include <algorithm>																								   
+#include <cmath>																									   
+#include <string>      																								   
+#include <time.h>																									   
+#include <iomanip> 																									   
+#include <sstream>																									   
+#include <unordered_map>																									   																							   
+																													   
+using namespace std; 																								   
+																													   
+const int INF = 1000000001;																						   
+const float PI = 3.14;																								   
+typedef long long LL;																								   
+																													   
+typedef vector<int> vi;																								   
+typedef list<int> li;																								   
+typedef queue<int> qi; 																								   
+typedef pair<int,int> ii; 																							   
+typedef map<string,int> msi; //np msi['nazwa'] = 'liczba'															   
+typedef vector<ii> vii; //vector par<int>, do tworzenia tablicy dla grafów wagowych #dijkstra #Bellman_Ford			   
+typedef priority_queue< ii, vector<ii>, greater<ii> > pq; //kolejka priorytetowa vectorów par(top()=min) #dijkstra	   
+																													   
+list<int>::iterator it;																								   
+list<int>::iterator it1;																							   
+vector<int>::iterator it2;																							   
+vector<int>::iterator it3;																							   
+set<int>::iterator it4;																								   
+map<string, int>::iterator it5; 																					   
+vector<ii>::iterator iter;																							   
+																													   																				   
+#define print_list(x) for(it = (x).begin(); it != (x).end(); it++) {cout << *it << " ";}							   
+#define print_vector(x) for(it2 = (x).begin(); it2 != (x).end(); it2++) {cout << *it2 << " ";}						   
+#define search_list(x) for(it = (x).begin(); it != (x).end(); it++)													   
+#define search_vector(x) for(it2 = (x).begin(); it2 != (x).end(); it2++);											   
+#define pb(x) push_back(x) 																							   
+#define pf(x) push_front(x)																							   
+#define mp(x, y) make_pair(x, y)                                                                                       
+///////////////////////////////////////////////GLOBAL DEFINITIONS////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////FUNCTIONS/////////////////////////////////////////////////////////////
+
+long long Hash_Function(string const& s) {
+    const int p = 31;  //P = 31
+    const int m = 1e9 + 9;
+    long long hash_value = 0;
+    long long p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
+
+/////////////////////////////////////////////////////MAIN////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])																					 
+{              												       
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   
+   string s;
+   cin >> s;
+   
+   LL wynik = Hash_Function(s);
+   unordered_map<LL, string> hash_tab;
+   unordered_map<LL, string>::iterator ite;
+   hash_tab.insert(mp(wynik, s));
+   
+   for(ite = hash_tab.begin(); ite != hash_tab.end(); ite++)
+   {
+	   cout << ite->first << " " << ite->second << endl;
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+	
+	return 0;
+}
