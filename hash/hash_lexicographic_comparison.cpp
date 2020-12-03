3cad95c23514b990a2739de5c3bf867bef790e27
Kacperfis <42153314+Kacperfis@users.noreply.github.com>
2020-12-03 15:15:20 +0100

    Update and rename lexicographic_comparison.cpp to hash_lexicographic_comparison.cpp

--- hash/hash_lexicographic_comparison.cpp
+++ hash/hash_lexicographic_comparison.cpp
@@ -14,6 +14,7 @@ typedef map<string,int> msi; //np msi['nazwa'] = 'liczba'
 typedef vector<ii> vii; //vector par<int>, do tworzenia tablicy dla grafów wagowych #dijkstra #Bellman_Ford			   
 typedef priority_queue< ii, vector<ii>, greater<ii> > pq; //kolejka priorytetowa vectorów par(top()=min) #dijkstra	   
 																													   																				   
+#define endl '\n'																													   																				   					
 																													   																				   
 #define print_list(x) for(it = (x).begin(); it != (x).end(); it++) {cout << *it << " ";}							   
 #define print_vector(x) for(it2 = (x).begin(); it2 != (x).end(); it2++) {cout << *it2 << " ";}						   
@@ -60,41 +61,52 @@ int main(int argc, char* argv[])
 	calculate_powers();
 	calculate_hash();
 	
-	//wczytanie podsłów do porównania leksykograficznego: [l1, r1]...[l2, r2]
-	int l1, l2, r1, r2;
-	cin >> l1 >> r1 >> l2 >> r2;
-	
-	string s1 = s.substr(l1, r1 - l1 + 1);
-	string s2 = s.substr(l2, r2 - l2 + 1);
-	//cout << "s1: " << s1 << endl; //podsłowo 1
-	//cout << "s2: " << s2 << endl; //podsłowo 2
-	int L = 0;
-	int R = min(s1.size(), s2.size());
-	
-	while(L < R){ //binary searchem wyszukujemy max długość wspólnego prefiksu obu podsłów
-		int mid = (L + R) / 2;
-		if(get_hash(l1, l1 + mid) == get_hash(l2, l2 + mid)){
-			L = mid + 1;
+	int tt;
+	cin >> tt;
+	while(tt--){
+		//wczytanie podsłów do porównania leksykograficznego: [l1, r1]...[l2, r2]
+		int l1, l2, r1, r2;
+		cin >> l1 >> r1 >> l2 >> r2;
+		
+		string s1 = s.substr(l1, r1 - l1 + 1);
+		string s2 = s.substr(l2, r2 - l2 + 1);
+		cout << "s1: " << s1 << endl; //podsłowo 1
+		cout << "s2: " << s2 << endl; //podsłowo 2
+		int L = 0;
+		//int R = min(s1.size(), s2.size());
+		int sajz1 = (r1-l1+1);
+		int sajz2 = (r2-l2+1);
+		int R = min(sajz1, sajz2);
+		
+		while(L < R){ //binary searchem wyszukujemy max długość wspólnego prefiksu obu podsłów
+			int mid = (L + R) / 2;
+			if(get_hash(l1, l1 + mid) == get_hash(l2, l2 + mid)){
+				L = mid + 1;
+			}
+			else R = mid;
 		}
-		else R = mid;
-	}
-	
-	int d = ((L + R) / 2); // d -> max długość wspólnego prefiksu obu podsłów
-	
-	if(d < min(s1.size(), s2.size())){
-		if(s[l1 + d] < s[l2 + d]){ //porównujemy litery podsłów na pozycjach d + 1
-			cout << "podsłowo " << s1 << " jest wcześniej leksykograficznie" << endl;
+		
+		int d = ((L + R) / 2); // d -> max długość wspólnego prefiksu obu podsłów
+		
+		if(get_hash(l1, r1) == get_hash(l2, r2)){
+			cout << "podsłowa są równe" << endl;
 		}
-		else if(s[l1 + d + 1] > s[l2 + d + 1]){
-			cout << "podsłowo " << s2 << " jest wcześniej leksykograficznie" << endl;
+		else{
+			if(d < min(sajz1, sajz2)){
+				if(s[l1 + d] < s[l2 + d]){ //porównujemy litery podsłów na pozycjach d + 1
+					cout << "podsłowo " << s1 << " jest wcześniej leksykograficznie" << endl;
+				}
+				else if(s[l1 + d] > s[l2 + d]){
+					cout << "podsłowo " << s2 << " jest wcześniej leksykograficznie" << endl;
+				}
+			}
+			else if(sajz1 < sajz2){
+				cout << "podsłowo " << s1 << " jest wcześniej leksykograficznie" << endl; 
+			}
+			else if(sajz1 > sajz2){
+				cout << "podsłowo " << s2 << " jest wcześniej leksykograficznie" << endl;
+			}
 		}
 	}
-	else if(s1.size() < s2.size()){
-		cout << "podsłowo " << s1 << " jest wcześniej leksykograficznie" << endl; 
-	}
-	else if(s1.size() > s2.size()){
-		cout << "podsłowo " << s2 << " jest wcześniej leksykograficznie" << endl;
-	}
 	
-}              												       
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
